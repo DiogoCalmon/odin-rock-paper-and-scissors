23a84b0d41c8c3b29301f4f3615b32843d8ef4d6
Diogo Calmon <diogocalmons@gmail.com>
2025-11-30 14:01:12 -0300

    Feat: Finish the game

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let scoreOfPlayer = 0;
 let scoreOfMachine = 0;
 
 for (let i = 0; i < 5; i++){
-    chooseOfPlayer = prompt("Digit 1 for choose rock\nDigit 2 for choose paper\nDigit 3 for choose scissor");
+    chooseOfPlayer = parseInt(prompt("Digit 1 for choose rock\nDigit 2 for choose paper\nDigit 3 for choose scissor"));
     chooseOfMachine = Math.floor(Math.random() * 3) + 1;
 
     switch (chooseOfPlayer){
@@ -40,36 +40,42 @@ for (let i = 0; i < 5; i++){
 
     if (chooseOfPlayer == "rock"){
         if (chooseOfMachine == "rock") {
-            alert("Draw");
+            alert(`Machine choose ${chooseOfMachine}\nDraw`);
         } else if (chooseOfMachine == "paper") {
-            alert("Machine won this match");
+            alert(`Machine choose ${chooseOfMachine}\nMachine won this match`);
             scoreOfMachine++;
         } else {
-            alert("Player won this match");
+            alert(`Machine choose ${chooseOfMachine}\nPlayer won this match`);
             scoreOfPlayer++;
         }
     } else if (chooseOfPlayer == "paper"){
         if (chooseOfMachine == "rock") {
-            alert("Player won this match");
+            alert(`Machine choose ${chooseOfMachine}\nPlayer won this match`);
             scoreOfPlayer++;
         } else if (chooseOfMachine == "paper") {
-            alert("Draw");
+            alert(`Machine choose ${chooseOfMachine}\nDraw`);
         } else {
-            alert("Machine won this match");
+            alert(`Machine choose ${chooseOfMachine}\nMachine won this match`);
             scoreOfMachine++;
         }
     } else {
         if (chooseOfMachine == "rock") {
-            alert("Machine won this match");
+            alert(`Machine choose ${chooseOfMachine}\nMachine won this match`);
             scoreOfMachine++;
         } else if (chooseOfMachine == "paper") {
-            alert("Player won this match");
+            alert(`Machine choose ${chooseOfMachine}\nPlayer won this match`);
             scoreOfPlayer++;
         } else {
-            alert("Draw");
+            alert(`Machine choose ${chooseOfMachine}\nDraw`);
         }
     }
+}
 
-    
+if (scoreOfPlayer > scoreOfMachine){
+    alert("Player won the game!!!")
+} else if (scoreOfMachine > scoreOfPlayer){
+    alert("Machine won the game")
+} else {
+    alert("The player and machine has the same score\nDRAW")
 }
 
